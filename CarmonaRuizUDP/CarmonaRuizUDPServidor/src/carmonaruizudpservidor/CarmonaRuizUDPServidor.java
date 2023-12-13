1eb4c19e7e12905151639fbad21525da69f81554
Juanma Carmona <juanmacarmona97@gmail.com>
2023-12-13 09:47:57 +0100

    uso UTF

--- CarmonaRuizUDP/CarmonaRuizUDPServidor/src/carmonaruizudpservidor/CarmonaRuizUDPServidor.java
+++ CarmonaRuizUDP/CarmonaRuizUDPServidor/src/carmonaruizudpservidor/CarmonaRuizUDPServidor.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 /**
@@ -29,7 +30,7 @@ public class CarmonaRuizUDPServidor {
             byte[] bufferLectura = new byte[3];
             DatagramPacket datagramaEntrada = new DatagramPacket(bufferLectura, bufferLectura.length);
             socket.receive(datagramaEntrada);
-            String letras = new String(bufferLectura, 0, datagramaEntrada.getLength());
+            String letras = new String(bufferLectura, StandardCharsets.UTF_8);
             System.out.println("(Servidor) Letras recibidas: " + letras);      
             String palabrasConLetra = obtenerPalabrasConLetra(letras);
             byte[] mensajeEnviado = palabrasConLetra.getBytes();
